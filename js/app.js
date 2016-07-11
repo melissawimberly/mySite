4c7acbf310b2f748a8fab9c33230422c9ef55946
melissawimberly <melissa.f.wimberly@gmail.com>
2016-07-11 17:35:12 -0400

    Defined a slides array on $scope for carousel images

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 angular.module('website', ['ngAnimate'])
-    .controller('MainCtrl', function ($scope) { });
+    .controller('MainCtrl', function ($scope) {
+    	$scope.slides = [
+    		{image: 'images/cover.jpg', description:' Cover Image'},
+    		{image: 'images/carousel/traveler.jpg', description:' Image'},
+    		{image: 'images/carousel/yogaSitting.jpg', description:' Image'},
+    		{image: 'images/yogaShapes.jpg', description:' Image'},
+    		{image: 'images/carousel/masonjars.jpg', description:' Image'},
+    		{image: 'images/sunglasses.jpg', description:' Image'},
+
+    	]
+     });
