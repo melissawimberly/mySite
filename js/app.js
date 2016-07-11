b20e663a552becedf2150ce82732ea37d5816c73
melissawimberly <melissa.f.wimberly@gmail.com>
2016-07-11 17:45:02 -0400

    Add hidding logic for images based on index

--- js/app.js
+++ js/app.js
@@ -6,7 +6,14 @@ angular.module('website', ['ngAnimate'])
     		{image: 'images/carousel/yogaSitting.jpg', description:' Image'},
     		{image: 'images/yogaShapes.jpg', description:' Image'},
     		{image: 'images/carousel/masonjars.jpg', description:' Image'},
-    		{image: 'images/sunglasses.jpg', description:' Image'},
+    		{image: 'images/sunglasses.jpg', description:' Image'}
 
-    	]
-     });
+    	];
+    $scope.currentIndex = 0;
+    $scope.setCurrentSlideIndex = function (index) {
+        $scope.currentIndex = index;
+    };
+    $scope.isCurrentSlideIndex = function (index) {
+        return $scope.currentIndex === index;
+    };
+});
