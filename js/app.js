8c900766e9b2f5544c962e5cbfa983607cec81cf
melissawimberly <melissa.f.wimberly@gmail.com>
2016-07-11 22:23:14 -0400

    Add routes for each section

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
-angular.module('website', ['ngAnimate'])
-    .controller('MainCtrl', function ($scope) {
-    	$scope.slides = [
-    		{image: 'images/cover.jpg', description:' Cover Image'},
-    		{image: 'images/carousel/traveler.jpg', description:' Image'},
-    		{image: 'images/carousel/yogaSitting.jpg', description:' Image'},
-    		{image: 'images/yogaShapes.jpg', description:' Image'},
-    		{image: 'images/carousel/masonjars.jpg', description:' Image'},
-    		{image: 'images/sunglasses.jpg', description:' Image'}
+var myApp = angular.module('myApp', [
+	'ngRoute',
+	'siteControllers'
+]);
 
-    	];
-    $scope.currentIndex = 0;
-    $scope.setCurrentSlideIndex = function (index) {
-        $scope.currentIndex = index;
-    };
-    $scope.isCurrentSlideIndex = function (index) {
-        return $scope.currentIndex === index;
-    };
-});
+myApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+	when('/home', {
+		templateUrl: 'partials/home.html',
+		controller: 'CarouselCtrl'
+	}).
+	when('/portfolio',{
+		templateUrl: 'partials/portfolio.html',
+		controller: 'PortfolioCtrl'
+	}).
+	when('/about', {
+		templateUrl: 'partials/about.html',
+		controller: 'AboutCtrl'
+	})
+	otherwise({
+		redirectTo: '/home'
+	});
+}]); 
